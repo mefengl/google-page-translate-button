78d65eb5f4d6f754858b3c893e3bbe4f9acd9171
冯磊 <71683364+mefengl@users.noreply.github.com>
2022-10-21 19:43:21 +0800

    :clown_face: Remove: not hide is the best hide

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         谷歌网页翻译
 // @namespace    https://github.com/mefengl
-// @version      1.3.4
+// @version      1.3.5
 // @description  🍓 一个按钮的事，一点都不费事
 // @author       mefengl
 // @match        http://*/*
@@ -20,14 +20,6 @@
         // if origin end with '.translate.goog'
         // then return
         if (origin.endsWith('.translate.goog')) {
-            // TODO: This should be an option that can be turned on
-            // move down the body and header
-            // $("body").css({ "padding-top": "56px" });
-            // $("header").css({ position: "fixed", top: 56 });
-            // TODO: Auto hide the header
-            // scroll down to hide the header
-            // scroll up to show the header
-            // because two header is too much...
             return;
         }
         // change '-' in origin to '--'

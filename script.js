839faa3e0e98bc78c3de54defda56e7a6acc561c
冯不游 <71683364+mefengl@users.noreply.github.com>
2022-12-23 04:14:12 +0800

    logic: new way to convert the href
    let google change it for us

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         谷歌网页翻译
 // @namespace    https://github.com/mefengl
-// @version      1.3.10
+// @version      2.0.0
 // @description  🍓 一个按钮的事，一点都不费事
 // @author       mefengl
 // @match        http://*/*
@@ -17,69 +17,24 @@
 (function () {
   "use strict";
   $(function () {
-    let origin = window.location.origin;
-    // if origin end with '.translate.goog'
-    // then return
-    if (origin.endsWith(".translate.goog")) {
-      return;
-    }
-    // change '-' in origin to '--'
-    origin = origin.replace(/-/g, "--");
-    // change '.' in origin to '-'
-    origin = origin.replace(/\./g, "-");
-    // append '.translate.goog' to origin
-    origin = origin + ".translate.goog";
+    // if origin end with '.translate.goog', then return
+    if (window.location.origin.endsWith(".translate.goog")) return;
 
-    const pathname = window.location.pathname;
-
-    let search = window.location.search;
-    // combine '?_x_tr_sl=auto&_x_tr_tl=zh-CN' and search
-    if (search) {
-      search = search + "&_x_tr_sl=auto&_x_tr_tl=zh-CN";
-    } else {
-      search = "?_x_tr_sl=auto&_x_tr_tl=zh-CN";
-    }
-
-    // combine origin, pathname and search
-    let url = origin + pathname + search;
-
-    // create a button
-    const $button = $("<button>翻译网页</button>");
-    // click to change url
-    $button.click(function () {
-      window.location.href = url;
-    });
-    // set button style
-    let hide_right = "-120px";
     // if title contains Chinese, then make button less visible
-    if (document.title.match(/[\u4e00-\u9fa5]/)) {
-      hide_right = "-130px";
-    }
-    $button.css({
-      position: "fixed",
-      width: "140px",
-      top: "120px",
-      right: hide_right,
-      "z-index": "999999",
-      "background-color": "#4285f4",
-      color: "#fff",
-      opacity: "0.8",
-      border: "none",
-      "border-radius": "4px",
-      padding: "10px 16px",
-      "font-size": "18px",
-      cursor: "pointer",
-    });
-    // hover to show, and hide when not hover
-    $button.hover(
-      function () {
-        $(this).stop().animate( { right: "-10px", }, 400);
-      },
-      function () {
-        $(this).stop().animate( { right: hide_right, }, 400);
-      }
-    );
-    // append button to body
-    $("body").append($button);
+    const hide_right = document.title.match(/[\u4e00-\u9fa5]/)
+      ? "-130px"
+      : "-120px";
+    // create the button
+    $("<button>翻译网页</button>")
+      .click(function () {
+        window.location.href = `https://translate.google.com/translate?sl=auto&tl=zh-CN&u=${window.location.href}`;
+      })
+      .css({ position: "fixed", width: "140px", top: "120px", right: hide_right, "z-index": "999999", "background-color": "#4285f4", color: "#fff", opacity: "0.8", border: "none", "border-radius": "4px", padding: "10px 16px", "font-size": "18px", cursor: "pointer", })
+      .hover(
+        // hover to show, and hide when not hover
+        function () { $(this).stop().animate({ right: "-10px", }, 400); },
+        function () { $(this).stop().animate({ right: hide_right, }, 400); }
+      )
+      .appendTo("body");
   });
 })();

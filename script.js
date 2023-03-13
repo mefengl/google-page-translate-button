aa01e0c80554a3e528c03905916f33fd5ab54af9
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-03-13 21:16:37 +0800

    fix(jquery): remove jquery

--- script.js
+++ script.js
@@ -1,46 +1,66 @@
 // ==UserScript==
 // @name         谷歌网页翻译
 // @namespace    https://github.com/mefengl
-// @version      2.0.2
+// @version      2.1.0
 // @description  🍓 一个按钮的事，一点都不费事
-// @author       mefengl
+// @author       mefengl & zyb19981014
 // @match        http://*/*
 // @match        https://*/*
-// @exclude      https://edition.cnn.com/
-// @exclude      https://www.baidu.com/
-// @exclude      https://bbs.jjwxc.net/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=translate.google.com
-// @require      https://cdn.staticfile.org/jquery/3.6.1/jquery.min.js
 // @grant        none
 // @license MIT
 // ==/UserScript==
 
 (function () {
   "use strict";
-  $(function () {
+
+  function createButton() {
     // if origin end with '.translate.goog', then return
     if (window.location.origin.endsWith(".translate.goog")) return;
 
     // if title contains Chinese, then make button less visible
-    const hide_right = document.title.match(/[\u4e00-\u9fa5]/)
-      ? "-130px"
-      : "-120px";
+    const hideRight = document.title.match(/[\u4e00-\u9fa5]/) ? "-130px" : "-120px";
+
     // create the button
-    const $button = $("<button>翻译网页</button>")
-      .click(function () {
-        window.location.href = `https://translate.google.com/translate?sl=auto&tl=zh-CN&u=${window.location.href}`;
-      })
-      .css({ position: "fixed", width: "140px", top: "120px", right: hide_right, "z-index": "999999", "background-color": "#4285f4", color: "#fff", opacity: "0.8", border: "none", "border-radius": "4px", padding: "10px 16px", "font-size": "18px", cursor: "pointer", })
-      .hover(
-        // hover to show, and hide when not hover
-        function () { $(this).stop().animate({ right: "-10px", }, 400); },
-        function () { $(this).stop().animate({ right: hide_right, }, 400); }
-      )
-      .appendTo("body");
+    const button = document.createElement("button");
+    button.innerHTML = "翻译网页";
+    button.style.position = "fixed";
+    button.style.width = "140px";
+    button.style.top = "120px";
+    button.style.right = hideRight;
+    button.style.zIndex = "999999";
+    button.style.backgroundColor = "#4285f4";
+    button.style.color = "#fff";
+    button.style.opacity = "0.8";
+    button.style.border = "none";
+    button.style.borderRadius = "4px";
+    button.style.padding = "10px 16px";
+    button.style.fontSize = "18px";
+    button.style.cursor = "pointer";
+    document.body.appendChild(button);
+
+    // hover to show, and hide when not hover
+    button.addEventListener("mouseenter", () => {
+      button.style.right = "-10px";
+    });
+    button.addEventListener("mouseleave", () => {
+      button.style.right = hideRight;
+    });
 
     // hide button if full screen
-    $(document).on("fullscreenchange", function () {
-      document.fullscreenElement ? $button.hide() : $button.show();
+    document.addEventListener("fullscreenchange", () => {
+      if (document.fullscreenElement) {
+        button.style.display = "none";
+      } else {
+        button.style.display = "block";
+      }
     });
-  });
+
+    // set button click action
+    button.addEventListener("click", () => {
+      window.location.href = `https://translate.google.com/translate?sl=auto&tl=zh-CN&u=${window.location.href}`;
+    });
+  }
+
+  window.addEventListener("load", createButton);
 })();
